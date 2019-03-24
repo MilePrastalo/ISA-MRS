3fb49bc24fd5d49e2fbd483ccd2c8b6ee1014279
Nikola Šarić <nikolasaric531@gmail.com>
2019-03-24 17:21:49 +0100

    initial commit

---
+++ PlanJourney/src/main/java/com/tim9/PlanJourney/PlanJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.tim9.PlanJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanJourneyApplication.class, args);
+	}
+
+}
