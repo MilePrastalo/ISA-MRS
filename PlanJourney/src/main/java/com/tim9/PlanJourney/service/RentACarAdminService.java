f2d6b9f37b42218e0b50c22544cffadc8c687ac4
MilePrastalo <mile.prastalo97@gmail.com>
2019-04-02 19:08:10 +0200

    rentacaradminrepository

---
+++ PlanJourney/src/main/java/com/tim9/PlanJourney/service/RentACarAdminService.java
@@ -0,0 +1,30 @@
+package com.tim9.PlanJourney.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tim9.PlanJourney.models.rentacar.RentACarAdmin;
+import com.tim9.PlanJourney.repository.RentACarAdminRepository;
+@Service
+public class RentACarAdminService {
+	@Autowired
+	private RentACarAdminRepository repository;
+	
+	public RentACarAdmin findOne(Long id) {
+		return repository.getOne(id);
+	}
+
+	public List<RentACarAdmin> findAll() {
+		return repository.findAll();
+	}
+	
+	public RentACarAdmin save(RentACarAdmin admin) {
+		return repository.save(admin);
+	}
+
+	public void remove(Long id) {
+		repository.deleteById(id);
+	}
+}
