429ec5cc47e0d2c7f0dd0e9d84ecb3961581ebd2
MilePrastalo <mile.prastalo97@gmail.com>
2019-04-16 14:03:26 +0200

    fixes

--- PlanJourney/src/main/java/com/tim9/PlanJourney/models/User.java
+++ PlanJourney/src/main/java/com/tim9/PlanJourney/models/User.java
@@ -3,15 +3,23 @@ package com.tim9.PlanJourney.models;
 import static javax.persistence.InheritanceType.TABLE_PER_CLASS;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
@@ -42,6 +50,12 @@ public abstract class User implements UserDetails{
 	
 	@Column(name = "last_password_reset_date")
     private Timestamp lastPasswordResetDate;
+	
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
 
 	public User() {
 	}
@@ -109,6 +123,14 @@ public abstract class User implements UserDetails{
 
     public void setLastPasswordResetDate(Timestamp lastPasswordResetDate) {
         this.lastPasswordResetDate = lastPasswordResetDate;
+    }
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
     }
 
 
