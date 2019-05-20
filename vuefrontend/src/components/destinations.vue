419e239b10b607e241c00588939822bb1a0996f7
jelenacuk <jelena.cuk.1997@gmail.com>
2019-05-20 14:33:38 +0200

    Editing and deleting flight company destinations. Detail view of flight reservation.

--- vuefrontend/src/components/destinations.vue
+++ vuefrontend/src/components/destinations.vue
@@ -8,15 +8,16 @@
                 <table  style="text-align: left">
                     <tr>
                         <td> Name: </td>
-                        <td>  <input type="text" name="DestName" v-model="DestName" > </td>
+                        <td> <input type="text" name="DestName" v-model="DestName" > </td>
                     </tr>
                     <tr>
                         <td> Coordinates: </td>
-                        <td>  <input type="text" name="DestCoordinates" v-model="DestCoordinates" > </td>
+                        <td > <input type="text" name="DestCoordinates" v-model="DestCoordinates" > </td>
                     </tr>
                     <tr>
                         <td> Description: </td>
-                        <td> <textarea  rows="5" cols="22" name="DestDescription"  v-model="DestDescription" style="overflow:scroll;"></textarea> </td>        
+                        <td> <textarea  rows="5" cols="22" name="DestDescription"  v-model="DestDescription" style="overflow:scroll;"></textarea> </td> 
+
                     </tr>
                     <tr>
                         <td></td>
@@ -37,12 +38,16 @@
                         <td>Description</td>
                         <td></td>
                     </tr>
-                    <tr v-for="destination in destinations" :key="destination.id">  
-                    <td>{{destination.name}}</td>
-                    <td>{{destination.coordinates}}</td>
-                    <td>{{destination.description}}</td>
-                    <td><button>Edit</button></td>
-                    <td><button>Delete</button></td>
+                    <tr v-for="(destination,index ) in destinations" :key="index">  
+                        <td v-if="editing == true"> <input type="text" name="DestName" v-model="destination.name" > </td>
+                        <td v-else>{{destination.name}}</td>
+                        <td v-if="editing == true"> <input type="text" name="DestCoordinates" v-model="destination.coordinates" > </td>
+                        <td v-else> {{destination.coordinates}} </td>
+                        <td v-if="editing == true"> <textarea  rows="5" cols="22" name="DestDescription"  v-model="destination.description" style="overflow:scroll;"></textarea> </td> 
+                        <td v-else>{{destination.description}}</td>
+                        <td  v-if="editing == false" @click="editDestination"><button>Edit</button></td>
+                        <td v-if="editing == true"><button @click="saveChanges(destination)">Save</button></td>
+                        <td><button @click="deleteDestination(index, destination)">Delete</button></td>
                     </tr>
                 </table>
                 </div>  
@@ -59,6 +64,7 @@ export default {
     DestCoordinates: "",
     DestDescription: "",
     destinations:[],
+    editing: false
   }
 },
 mounted(){
@@ -73,19 +79,56 @@ mounted(){
     },
     methods:{
         addNewDestination: function(){
+            if ( this.DestName == "" ||  this.DestCoordinates == "" || this.DestDescription == ""){
+                alert("Fields can not be empty!");
+                return;
+            }
             var newDestination = {name:this.DestName, description: this.DestDescription, coordinates: this.DestCoordinates}
             var getJwtToken = function() {
                 return localStorage.getItem('jwtToken');
             };
-            alert(this.DestName);
             console.log(newDestination);
             axios.defaults.headers.common['Authorization'] = "Bearer " + getJwtToken();
              axios.post("http://localhost:8080/api/addDestination",newDestination)
             .then(response => {
                 alert("New destination added!")
+                this.destinations.push(response.data);
+                this.DestName = this.DestCoordinates = this.DestDescription = "";
             });
             
-        }        
+        },
+        editDestination: function(){
+            this.editing = true;
+        },
+        
+        saveChanges: function(destination){
+
+            if ( this.DestName == "" ||  this.DestCoordinates == "" || this.DestDescription == ""){
+                alert("Fields can not be empty!");
+                return;
+            }
+            var getJwtToken = function() {
+                return localStorage.getItem('jwtToken');
+            };
+            axios.defaults.headers.common['Authorization'] = "Bearer " + getJwtToken();
+            axios.post("http://localhost:8080/api/editDestination",destination)
+            .then(response => {
+                alert(response.data)
+            });
+            this.editing = false;
+        },
+
+        deleteDestination: function(index, destination){
+            var getJwtToken = function() {
+                return localStorage.getItem('jwtToken');
+            };
+            axios.defaults.headers.common['Authorization'] = "Bearer " + getJwtToken();
+            axios.get("http://localhost:8080/api/removeDestination" + "/" + destination.id)
+            .then(response => {
+                alert(response.data)
+            });
+            this.destinations.splice(index,1);
+        }
     }
 }
 
