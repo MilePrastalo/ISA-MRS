7a4df4384224394330809ae5e90b685f9a407a8d
Nikola Saric <nikolasaric531@gmail.com>
2019-06-10 17:07:27 +0200

    System admin now can add new system admins.

---
+++ vuefrontend/src/components/SystemAdmin/saAddSystemAdmin.vue
@@ -0,0 +1,101 @@
+<template>
+   <div id = "saAddSystemAdmin" class="container">
+       <br>
+       <div class="row centered">
+       <table>
+                <tr>
+                    <td><b> Username: </b></td>
+                    <td>  <input type="text" v-model="newSysAdmin.username" > </td>
+                </tr>
+                <tr>
+                    <td><b> Password: </b></td>
+                    <td>  <input type="text" v-model="newSysAdmin.password" > </td>
+                </tr>
+                <tr>
+                    <td><b> First Name: </b></td>
+                    <td> <input type="text" v-model="newSysAdmin.firstName" > </td>        
+                </tr>
+                <tr>
+                    <td><b> Last Name: </b></td>
+                    <td> <input type="text" v-model="newSysAdmin.lastName" > </td>        
+                </tr>
+                <tr>
+                    <td><b> Email: </b></td>
+                    <td> <input type="text" v-model="newSysAdmin.email" > </td>        
+                </tr>
+                <tr>
+                    <td>  </td>
+                    <td><button v-on:click="addSystemAdmin()" class="btn-primary">Add Hotel Admin</button> </td>      
+                </tr>
+        </table>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'saAddSystemAdmin',
+  components: {
+  },
+  data: function () {
+  return {
+    newSysAdmin: {}
+  }
+},
+mounted(){
+        var getJwtToken = function() {
+            return localStorage.getItem('jwtToken');
+        };
+        axios.defaults.headers.common['Authorization'] = "Bearer " + getJwtToken();
+    },
+    methods:{
+        addSystemAdmin: function() {
+            if(this.newSysAdmin.username == "") {
+                alert("Username must not be empty.");
+                return;
+            }
+            if(this.newSysAdmin.firstName == "") {
+                alert("Firstname must not be empty.");
+                return;
+            }
+            if(this.newSysAdmin.lastName == "") {
+                alert("Lastname must not be empty.");
+                return;
+            }
+            if(this.newSysAdmin.email == "") {
+                alert("Email must not be empty.");
+                return;
+            }
+            if(this.newSysAdmin.password == "") {
+                alert("Password must not be empty.");
+                return;
+            }
+
+            axios.post("http://localhost:8080/api/addSystemAdmin",this.newSysAdmin)
+            .then(response => {
+                if(response.data == false) {
+                    alert("Username is taken.");
+                    return;
+                } else {
+                    alert("System admin has been successfully added.");
+                    return;
+                }
+            })
+        }
+    }
+        
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-left: 5%;
+}
+</style>
